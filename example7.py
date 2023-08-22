126d569d188330df81d84e6cbdf96b07bd67c100
Nishanaaz <nishan22@navgurukul.org>
2023-08-22 16:39:55 +0530

    initial commit

--- example7.py
+++ example7.py
@@ -16,7 +16,7 @@ class c(b):
 obj3=c()
 print(obj3.m1())
 
-# keypoints:
-1-jab obj3.m1 ko call karega tab class c m jaega and then
-aur vhaa par self.m1 ko baar baar call kr rha hai to recursion ban gya
-kyonki koi vbase condition nahin hai.
+# # keypoints:
+# 1-jab obj3.m1 ko call karega tab class c m jaega and then
+# aur vhaa par self.m1 ko baar baar call kr rha hai to recursion ban gya
+# kyonki koi vbase condition nahin hai.
